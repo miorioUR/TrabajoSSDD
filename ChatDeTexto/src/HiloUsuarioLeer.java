5004e3a3ccd48b5131362945119edd6a03101576
Don Migol <migolunirioja@gmail.com>
2022-01-11 01:06:54 +0100

    Version actualizada
    Funcional pero con 3 errores principales
    
    - Mensajes se envian 2 veces
    - Error al /disconnect con cierre de sockets
    - Mensaje de error en el server cuando un cliente sale
    
    - Tampoco se consigue compatibilidad en librerias

---
+++ ChatDeTexto/src/HiloUsuarioLeer.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.HashMap;
+
+
+
+public class HiloUsuarioLeer extends Thread{
+
+	DataInputStream teclado = new DataInputStream(System.in);
+	Socket s;
+	String nombre;
+	String idioma;
+	
+	public HiloUsuarioLeer(Socket soc, String nom,String id) { //En el constructor se ponen nombre e idioma
+			this.s = soc;
+			this.nombre = nom;
+			this.idioma = id;
+	}
+	public void run() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(this.s.getInputStream(),"UTF-8"));
+			
+			//Translator translate = Translator.getInstance();
+			//quiero hacer referencia a java-google-text-to-speech-master/bin/com/gtranslate/Translator.class
+			
+			String recibido = br.readLine();
+			
+			while(recibido!=null) {	
+				
+				//String text = translate.translate(recibido, Language.ENGLISH, Language.PORTUGUESE); 
+				//System.out.println(text);
+				
+				System.out.println(recibido); //Se esta ejecutando 2 veces, pero no he creado 2 hiloleer?
+				
+				recibido=br.readLine();
+			}
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String getIdioma(String s) {
+		switch(s) {
+		case "EN":
+			return "Language.ENGLISH";
+			
+			//etc añadir añadir
+		
+		}
+		return "Language.ENGLISH";
+	}
+}
